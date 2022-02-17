530290b3fee7489ce166363105aa34fbe6601a9f
CAMILA ALVES FERREIRA <camila.aferreira22@senacsp.edu.br>
2022-02-16 23:11:36 -0200

    Aula 01

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# senac-js-2201
+
+Scripts para Web 2022 - 01
+
+##Oba, JavaScript! ##
+
+Vamos nos divertir muuuuuito!!!
+
---
+++ app.js
@@ -0,0 +1,33 @@
+//Comentário
+/*Comentário de bloco
+ */
+
+const XPTO = 10;
+
+console.log(XPTO);
+
+let minhaVar = "Olá";
+
+minhaVar = minhaVar + " Mundo!";
+
+console.log(minhaVar);
+
+function aula1(){
+    
+    if(true) {
+        console.log("Parabéns")
+    }else{
+        console.log("Errou!");
+    }
+
+    for(i = 1; i < 10; i++){
+        console.log(i);
+    }
+}
+
+aula1();
+
+function diretoNoElemento(){
+    console.log('Olá Mundo');
+}
+
