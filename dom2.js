af97f34dfdc3a55b777ffb993d3c6d68404ea8ef
CAMILA ALVES FERREIRA <camila.aferreira22@senacsp.edu.br>
2022-03-16 22:20:58 -0300

    dom part 2

--- dom2.js
+++ dom2.js
@@ -1,11 +1,13 @@
 const p = document.getElementById('paragrafo');
+const titulo = document.querySelector('h1');
+
+console.log(titulo);
 
 p.innerText = 'Meu texto!';
 
 if(confirm('Quer entrar com um valor?')){
 
     let valor = prompt('Digite um número de 0 a 10');
-
 
     if(valor >= 0 && valor<=10){
 
@@ -14,11 +16,16 @@ if(confirm('Quer entrar com um valor?')){
         for(i=1; i <= valor; i++){
             p.innerText = p.innerText + 'Camila\n';
         }
+
+        titulo.style.background = 'green';
+
     }else{
         p.innerText = 'Valor inválido';
+        titulo.style.background = 'yellow';
     }
 }else{
     p.innerText = 'Que pena que você não entrou com um valor :c';
+    titulo.style.background = 'red';
 }
     
 
