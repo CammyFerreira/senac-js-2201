713486df10279b869b26155e6910a75bbf2bc94d
CamilaAFerreira <camila.aferreira@tksolution.com.br>
2022-03-27 21:54:21 -0300

    Aula 04

--- dom.js
+++ dom.js
@@ -1,13 +1,15 @@
 // DOM - Document Object Model
 
+//getElementByID - Retorna a referência do elemento através do seu ID.
 const p = document.getElementById('paragrafo');
 
+//mudando o texto do parágrafo
 p.innerText = 'Meu texto!';
+//mudando a cor de fundo do parágrafo
+p.style.background = 'red';
 
-// p.style.background = 'red';
 
-
-
+//Exercício 
 const v = ['red', 'purple', 'blue', 'yellow', 'pink'];
 
 
@@ -16,7 +18,7 @@ for(i=0, c = 0; i<1000; i++, c++){
     p.style.background = v[c];
 
     console.log(c + ': ' + p.style.background);
-
+    
     if(c >= 5) c = -1;
 }
 
