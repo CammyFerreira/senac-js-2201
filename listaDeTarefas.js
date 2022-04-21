0b9956b8e401e823849caf6333deb2a921ce817d
CAMILA ALVES FERREIRA <camila.aferreira22@senacsp.edu.br>
2022-04-20 22:08:23 -0300

    LocalStorage

---
+++ listaDeTarefas.js
@@ -0,0 +1,106 @@
+const FORMULARIO = document.querySelector('#form-tarefa');
+const LISTA = document.querySelector('.collection');
+const BTN_LIMPAR = document.querySelector('.limpar-tarefas');
+const FILTRO = document.querySelector('#filtro');
+const CAMPO_TAREFA = document.querySelector('#tarefa');
+
+function carregaMonitorDeEventos() {
+
+    //Eventos
+    FORMULARIO.addEventListener('submit', adicionarTarefa);
+    LISTA.addEventListener('click', apagaTarefa);
+    BTN_LIMPAR.addEventListener('click', apagaTodasTarefa);
+    FILTRO.addEventListener('click', filtraTarefa);
+}
+function pegaTarefa() {
+    let tarefas;
+
+    if (localStorage.getItem('tarefas') === null) {
+        tarefas = [];
+    } else {
+        tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    }
+
+    tarefas.forEach(function () {
+        const LI = document.createElement('li');
+        LI.className = 'collection-item';
+        LI.appendChild(document.createTextNode(tarefas));
+        const A = document.createElement('a');
+        A.className = 'apaga-tarefa secondary-content';
+        const I = document.createElement('i');
+        I.className = 'fa fa-remove';
+        A.appendChild(I);
+        LI.appendChild(A);
+        LISTA.appendChild(LI);
+    });
+}
+function filtraTarefa(evento) {
+
+    const texto = evento.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function (tarefa) {
+
+        const item = tarefa.firstChild.textContent;
+        //-1 significa que não há o que está sendo buscado
+        if (item.toLowerCase().indexOf(texto) != -1) {
+            tarefa.style.display = 'block';
+        } else {
+            tarefa.style.display = 'none'
+        }
+    });
+}
+
+
+function apagaTodasTarefa(evento) {
+
+    evento.preventDefault();
+
+    LISTA.innerHTML = '';
+}
+
+function apagaTarefa(evento) {
+
+    if (evento.target.parentElement.classList.contains('apaga-tarefa')) {
+        evento.target.parentElement.parentElement.remove();
+    }
+}
+
+function adicionarTarefa(evento) {
+    evento.preventDefault();
+    if (CAMPO_TAREFA.value === '') {//Verifica se o campo está vazio
+        alert('Insira uma tarefa');
+    } else {
+        //CRIA OS NOVOS ELEMENTOS
+        const LI = document.createElement('li');
+        LI.className = 'collection-item';
+        LI.appendChild(document.createTextNode(CAMPO_TAREFA.value));
+        const A = document.createElement('a');
+        A.className = 'apaga-tarefa secondary-content';
+        const I = document.createElement('i');
+        I.className = 'fa fa-remove';
+        A.appendChild(I);
+        LI.appendChild(A);
+
+        LISTA.appendChild(LI);
+
+        gravaTarefaNoLocalStorage(CAMPO_TAREFA);
+        CAMPO_TAREFA.value = '';
+
+    }
+
+
+}
+
+function gravaTarefaNoLocalStorage(tarefa) {
+    let tarefas;
+
+    if (localStorage.getItem('tarefa') === null) {
+        tarefas = [];
+    } else {
+        tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    }
+    tarefas.push(tarefa.value);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+carregaMonitorDeEventos();
+
--- localStorage.js
+++ localStorage.js
@@ -3,30 +3,53 @@ const OBJ = {nome: "Camila", idade: 23, fazCompra: function(){
     console.log('Comprou alguma coisa');
 }};
 
-console.log(OBJ);
+// console.log(OBJ);
 
 // OBJ.fazCompra();
 
 const STR = '{"nome": "Camila", "idade": 37}';
 
-console.log(JSON.parse(STR));
+//console.log(JSON.parse(STR));
 
 localStorage.setItem('nome', 'Camila');
 
 const NOME = localStorage.getItem('nome');
 
-console.log(NOME);
+// console.log(NOME);
 
 sessionStorage.setItem('idade', '37');
 
 const IDADE = sessionStorage.getItem('idade');
 
-console.log(IDADE);
-
+// console.log(IDADE);
+//recupera valor do input preenchido pelo usuário e adiciona no storage
 document.querySelector('input.btn').addEventListener('click', function(e){
-    e.preventDefault();
-    const TAREFA = document.querySelector('#task').value;
-    document.querySelector('#task').value = '';
-    localStorage.setItem('tarefa', TAREFA);
 
+//prevenir o comportamento padrão do botão
+    e.preventDefault();
+
+    const TAREFA = document.querySelector('#task').value;
+    
+    let tarefas;
+
+    if(localStorage.getItem('tarefas') === null){
+
+        tarefas =[];
+
+    }else{
+
+        tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    }
+
+//push - 
+
+    tarefas.push(TAREFA);
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    
+    console.log(tarefas);
+
+    document.getElementById('task').value = '';
 });
+
