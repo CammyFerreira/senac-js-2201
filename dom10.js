86ece8ea9e1f3d8d81cf6b16c24d68fb32cf0321
Camila Alves <91034827+CammyFerreira@users.noreply.github.com>
2022-06-01 13:49:32 -0300

    Update dom10.js

--- dom10.js
+++ dom10.js
@@ -1,3 +1,5 @@
+//se o if for true ele remove o elemento pai do elemento pai daquele elemento;
+
 document.body.addEventListener('click', function(e){
     if(e.target.parentElement.classList.contains('delete-item') === true){
         e.target.parentElement.parentElement.remove();
